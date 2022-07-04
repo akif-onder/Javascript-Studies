6f1c9958e9d3ac7c5f369663e98a34df003367aa
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-07-04 12:21:10 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// let merhaba = function () {
+//     console.log('bu bir callback');
+// };
+
+setTimeout(function () {
+    console.log('bu bir callback. ama anonymous func yazdım içine');
+}, 3000);
+console.log('naber');
