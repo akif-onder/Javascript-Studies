72c1ff3a9d76f3684551ca481d25a4fdf9e817df
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-07-04 12:36:45 +0300

    new examples

--- app.js
+++ app.js
@@ -2,7 +2,16 @@
 //     console.log('bu bir callback');
 // };
 
-setTimeout(function () {
-    console.log('bu bir callback. ama anonymous func yazdım içine');
-}, 3000);
-console.log('naber');
+// setTimeout(function () {
+//     console.log('bu bir callback. ama anonymous func yazdım içine');
+// }, 3000);
+// console.log('naber');
+
+// document.querySelector('#btn').addEventListener('click', function () {
+//     console.log('Butona tıklandı');
+// });
+
+document.querySelector('#btn').addEventListener('click', 
+ () =>console.log('Butona tıklandı'));
+
+console.log('Naber');
