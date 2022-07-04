48a37f3745fcbce4a94f6e25b93aa0a34307689d
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-07-04 12:54:06 +0300

    functions described

--- app.js
+++ app.js
@@ -11,7 +11,40 @@
 //     console.log('Butona tıklandı');
 // });
 
-document.querySelector('#btn').addEventListener('click', 
- () =>console.log('Butona tıklandı'));
+// document.querySelector('#btn').addEventListener('click', 
+//  () =>console.log('Butona tıklandı'));
 
-console.log('Naber');
+// console.log('Naber');
+
+
+let posts = [
+    {
+        "title": "Title 1",
+        "body": "Body 1"
+    },
+
+    {
+        "title": "Title 2",
+        "body": "Body 2"
+    }
+];
+
+function addPost() {
+    setTimeout(function (){
+        posts.push({"title": "Title 3", "body": "Body 3"});
+    }, 3000);
+}
+
+function getAllPosts() {
+    setTimeout(function (){
+        let output = "<ul>";
+        posts.forEach(function (post) {
+            output += `<li>${post.title}- ${post.body}</li>`;
+        })
+        output += "</ul>";
+        document.getElementById("output").innerHTML = output
+    }, 4000);
+}
+
+addPost();
+getAllPosts();
